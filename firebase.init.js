fc2f0996179aba1573757833f423ab8adf92f4b3
marium-asim <54772913+marium-asim@users.noreply.github.com>
2020-10-07 02:47:28 +0500

    Add files via upload

---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC80PeRuIszOg35iHjRV3MazZLzk0Ludig",
+    authDomain: "chatapp-c86a6.firebaseapp.com",
+    databaseURL: "https://chatapp-c86a6.firebaseio.com",
+    projectId: "chatapp-c86a6",
+    storageBucket: "chatapp-c86a6.appspot.com",
+    messagingSenderId: "441508104908",
+    appId: "1:441508104908:web:18fa20d67e74f993b49069"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
